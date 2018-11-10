29ba3302c7c888b3d595a0d5b3782c59f693d0d8
浅忆梦微凉 <3538922590@qq.com>
2018-11-10 20:50:37 +0800

    失败2

--- 长度最小的子数组/test.c
+++ 长度最小的子数组/test.c
@@ -11,18 +11,18 @@
 #include<Windows.h>
 
 int minSubArrayLen(int s, int* nums, int numsSize) {
+	int k = 0;
 	int i = 0;
 	int j = 0;
-	int k = 0;
-	for (i = 2; i < numsSize; i++)
+	for (k = 2; k < numsSize; k++)
 	{
-		for (j = 0; j < numsSize-i; j++)
+		for (i = 0; i <= numsSize - k; i++)
 		{
-			for (k = j + 1; k < j+i; k++)
+			for (j = i + 1; j < i + k; j++)
 			{
-				if (nums[j] + nums[k] == s)
+				if ((nums[i] + nums[j]) == s)
 				{
-					return i;
+					return k;
 				}
 			}
 		}
@@ -32,7 +32,7 @@ int minSubArrayLen(int s, int* nums, int numsSize) {
 
 int main()
 {
-	int nums[] = { 2, 3, 1, 2, 4, 3 };
+	int nums[] = { 2, 3, 1, 2, 2,2, 3 };
 	int s = 7;
 	int numsSize = sizeof(nums) / sizeof(nums[0]);
 	int ret = minSubArrayLen(s, nums, numsSize);
